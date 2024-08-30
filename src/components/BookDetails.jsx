a930effa42a1ee92ff30b2a016b0745084dba296
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-08-30 18:53:31 +0530

    minor update

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -2,9 +2,11 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux"; 
 import BookDetail from "./BookDetail";
+import Error from './Error'
 
 export default function BookDetails() {
   const [pick, setPick] = useState(null);
+  // const [errorpick,setErrorPick]=useState('')
   const params = useParams();
   
 
@@ -13,7 +15,14 @@ export default function BookDetails() {
   useEffect(() => {
     const Picked = books.find((p) => p.title.toLowerCase() === params.title.toLowerCase());
     setPick(Picked);
+    // setErrorPick()
   }, [params.title, books]); 
+  // console.log('pick', pick.title);
+  // console.log('params',params.title);
+  if (!pick)
+  {  
+    return <Error></Error>
+  }
 
   return (
     <>
